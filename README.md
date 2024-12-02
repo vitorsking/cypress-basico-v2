c9dccb4133d8d34fdbe1b31c763df43088668a7b
MachadoProg <vitormachadobdo@gmail.com>
2024-12-02 20:35:43 -0300

    Create cypress project

--- README.md
+++ README.md
@@ -1,29 +1,28 @@
-# Testes automatizados com Cypress - Básico
+# Teste automatizado
 
-👋 Seja bem-vindo(a)!
+# Pré-requisitos
 
-É muito bom tê-lo(a) aqui. Tenho certeza que você vai adorar este curso. ❤️
+-   [git](https://git-scm.com/) (estou usando a versão `2.34.1`)
+-   [Node.js](https://nodejs.org/en/) (estou usando a versão `v16.13.2` )
+-   npm (estou usando a versão `8.3.2`)
+-   [Google Chrome](https://www.google.com/intl/pt_br/chrome/) (estou usando a versão `98.0.4758.80 (Official Build) (x86_64)`)
 
-## O que você vai aprender
+> **Obs.:** Recomendo utilizar as mesmas versões, ou versões mais recentes dos sistemas listados acima.
+>
+> **Obs. 2:** Ao instalar o Node.js o npm é instalado junto. 🎉
+>
+> **Obs. 3:** Para verificar as versões do git, Node.js e npm instaladas em seu computador, execute o comando `git --version && node --version && npm --version` no seu terminal de linha de comando.
 
-Durante o curso de testes automatizados com Cypress (básico), você vai aprender:
+It is required to have Node.js and npm installed to run this project.
 
-- Como configurar um projeto Cypress do zero
-- Como visitar páginas locais e remotas
-- Como lidar com os elementos mais comuns encontrados em aplicações web
-- Como testar _upload_ de arquivos
-- Como realizar as mais diversas verificações de resultados esperados
-- Como criar comandos customizados
-- Como lidar com links que abrem em outra aba do navegador
-- Como rodar testes simulando as dimensões de um dispositivo móvel
-- Como resolver os mesmos problemas de diferentes formas, conhecendo a [API do Cypress](https://docs.cypress.io/api/table-of-contents)
-- Como executar os testes em um _pipeline_ de integração contínua sempre que mudanças ocorrerem no código da aplicação (ou dos testes)
-- Como criar uma documentação mínima para seu projeto de testes automatizados
+> I used versions `v18.15.0` and `9.5.0` of Node.js and npm, respectively. I suggest you use the same or later versions.
 
-## Vamos começar?
+## Installation
 
-Vá para a seção [estrutura do curso](./lessons/_course-structure_.md).
+Run `npm install` (or `npm i` for the short version) to install the dev dependencies.
 
-___
+## Tests
 
-Este é mais um curso da [**Escola Talking About Testing**](https://udemy.com/user/walmyr).
+Run `npm test` (or `npm t` for the short version) to run the test in headless mode.
+
+Or, run `npm run cy:open` to open Cypress in interactive mode.
--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -37,10 +37,9 @@ describe("Central de Atendimento ao Cliente TAT", function () {
         cy.get("#firstName").type("Vitor");
         cy.get("#lastName").type("Machado");
         cy.get("#email").type("vitor@teste.com");
-        cy.get("#phone-checkbox").click();
+        cy.get("#phone-checkbox").check();
         cy.get("#open-text-area").type("teste");
         cy.contains("button", "Enviar").click();
-
         cy.get(".error").should("be.visible");
     });
     ///preenche e limpa
@@ -77,5 +76,79 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     it("envia o formuário com sucesso usando um comando customizado", function () {
         cy.fillMandatoryFieldsAndSubmit();
         cy.get(".success").should("be.visible");
+    });
+
+    it("Seleciona um produto", function () {
+        cy.get("#product").select("YouTube").should("have.value", "youtube");
+    });
+
+    it("selecionar  um produto pelo valor(value)", function () {
+        cy.get("#product").select("mentoria").should("have.value", "mentoria");
+    });
+
+    it("valor pelo indice", function () {
+        cy.get("#product").select(1).should("have.value", "blog");
+    });
+
+    it("Marcar tipo de atendimento", function () {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should("have.value", "feedback");
+    });
+
+    it("Marca cada tipo de radio", function () {
+        cy.get('input[type="radio"]')
+            .should("have.length", 3)
+            .each(function ($radio) {
+                cy.wrap($radio).check();
+                cy.wrap($radio).should("be.checked");
+            });
+    });
+
+    it("Marca ambos check box, desmarca ultimo", function () {
+        cy.get('input[type="checkbox"]')
+            .check()
+            .should("be.checked")
+            .last()
+            .uncheck()
+            .should("not.be.checked");
+    });
+
+    it("Fazendo upload de arquivos", function () {
+        cy.get('input[type="file"]')
+            .should("not.have.value")
+            .selectFile("./cypress/fixtures/example.json")
+            .should(function ($input) {
+                expect($input[0].files[0].name).to.equal("example.json");
+            });
+    });
+
+    it("teste simulando drag-and-drop", function () {
+        cy.get('input[type="file"]')
+            .should("not.have.value")
+            .selectFile("./cypress/fixtures/example.json", {
+                action: "drag-drop",
+            })
+            .should(function ($input) {
+                expect($input[0].files[0].name).to.equal("example.json");
+            });
+    });
+
+    it("selecione um arquivo usando uma fixture", () => {
+        cy.fixture("example.json").as("sampleFile");
+        cy.get('input[type="file"]')
+            .selectFile("@sampleFile")
+            .should(function ($input) {
+                expect($input[0].files[0].name).to.equal("example.json");
+            });
+    });
+
+    it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", () => {
+        cy.get("#privacy a").should("have.attr", "target", "_blank");
+    });
+
+    it("acessa a página da política de privacidade removendo o target e então clicando no link", () => {
+        cy.get("#privacy a").invoke("removeAttr", "target").click();
+        cy.contains("Talking About Testing").should("be.visible");
     });
 });
